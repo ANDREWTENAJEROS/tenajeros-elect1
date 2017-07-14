80eb9e07bd23ccff440abea5928fd516526679d2
andrewtenajeros <jules.tenajeros@gmail.com>
2017-07-14 12:02:07 +0800

    2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 //set port
 app.set('port', (process.env.PORT || 5000));
 //use static files
-app.use(express.static(path.join(__dirname, 'public/')));
+app.use(express.static(path.join(__dirname, 'public')));
 //express routes
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index2.html'));
@@ -17,8 +17,6 @@ app.get('/index2.html', function(req, res){
 app.get('/index0.html', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index0.html'));
 });
-
-
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
